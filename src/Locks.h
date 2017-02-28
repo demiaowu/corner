7af54a53c4319c73592178da01549c8390128210
demiaowu <demiaowu@163.com>
2017-03-01 00:20:24 +0800

    add Locks.h which include all header file about any type of lock

---
+++ src/Locks.h
@@ -0,0 +1,15 @@
+//
+// Created by demiaowu on 17-3-1.
+//
+
+#ifndef MYTEST_LOCKS_H
+#define MYTEST_LOCKS_H
+
+#include "FastLock.h"
+#include "MutexLock.h"
+#include "MutexLockGuard.h"
+#include "Condition.h"
+#include "CountDownLatch.h"
+#include "RWLock.h"
+
+#endif //MYTEST_LOCKS_H
